6b365443bf04c7efacf27dff728ccabd24690cc8
Hau <hauvo1709@gmail.com>
2021-11-08 14:36:17 +0700

    organizing folder structure

--- meta_poly_client_v0/src/App.js
+++ meta_poly_client_v0/src/App.js
@@ -1,9 +1,14 @@
 function App() {
 
-
     return (
         <div className="App-Container">
+            <div className="App-Inner-Container">
+                <div className="App-Wrapper">
+                    <div className="App-Inner-Wrapper">
 
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
